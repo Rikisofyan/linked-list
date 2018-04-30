6c76c2eb4605e59875a804f68a7449bdb068c526
Rikisofyan <38391318+Rikisofyan@users.noreply.github.com>
2018-04-30 18:54:06 +0700

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -18,3 +18,54 @@ int isEmpty(){  //Fungsi mengetahui apakah Linked list kosong atau ada isinya
 if(tail == NULL) return 1;
 else return 0;
 }
+void insertDepan(int databaru){
+TNode *baru;
+baru = new TNode;
+baru->data = databaru;
+baru->next = NULL;
+if(isEmpty()==1){
+head=tail=baru;
+tail->next=NULL;
+}
+else {
+baru->next = head;
+head = baru;
+}
+printf("Data masuk\n");
+}
+
+void tambahTengah(int databaru){
+TNode *baru,*bantu;
+baru = new TNode;
+baru->data = databaru;
+baru->next = NULL;
+if(isEmpty()==1){
+head=baru;
+tail=baru;
+tail->next = NULL;
+}
+else {
+tail->next = baru;
+tail=baru;
+}
+printf("Data masuk\n");
+}
+
+
+void tambahBelakang(int databaru){
+TNode *baru,*bantu;
+baru = new TNode;
+baru->data = databaru;
+baru->next = NULL;
+if(isEmpty()==1){
+head=baru;
+tail=baru;
+tail->next = NULL;
+}
+  
+else {
+tail->next = baru;
+tail=baru;
+}
+printf("Data masuk\n");
+}
